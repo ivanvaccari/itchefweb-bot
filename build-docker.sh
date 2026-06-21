8598d022a3c3d6c424585a960b3dc4f7af435532
ivanvaccari <vaccari87@gmail.com>
2026-06-21 15:55:52 +0200

    Minor updates

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-IMAGENAME="ivaccari/itchefweb-bot:1.0.0"
+# fix git-bash expansion su windows
+export MSYS_NO_PATHCONV=1
+
+PACKAGEVERSION=$(node -p "require('./package.json').version")
+IMAGENAME="ivaccari/itchefweb-bot:$PACKAGEVERSION"
 
 rm -rf dist
 npm run build
 docker build -t $IMAGENAME .
+
+# Ask for push 
+echo "Pushare immagine su dockerhub? (y/n)"
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    docker push $IMAGENAME
+fi
--- src/environment.ts
+++ src/environment.ts
@@ -1,13 +1,24 @@
 import "dotenv/config";
 
 export const environment = {
+    /**
+     * Token di telegram, da generare tramite BotFather
+     */
     telegramBotToken: process.env.TELEGRAM_BOT_TOKEN || "",
-    // Il cron che gira alle 9:00  verifica le prenotazioni per oggi
+    
+    /**
+     * Il cron che gira alle 9:00  verifica le prenotazioni per oggi
+     */
     cronToday: process.env.CRON || "00 9 * * *",
-    // Il cron che gira alle 14:30 verifica le prenotazioni per domani
+
+    /**
+     * Il cron che gira alle 14:30 verifica le prenotazioni per domani
+     */
     cronTomorrow: process.env.CRON_TOMORROW || "30 14 * * *", 
 
-    // URl base dell'applicazione Camst, ad esempio: https://itchefwebcl.camst.it/XXXXXX/ITChefWebApp
-    // Puoi trovare questo url aprendo il portale Camst nel browser e copiando l'url fino a /ITChefWebApp
+    /**
+     * URl base dell'applicazione Camst, ad esempio: https://itchefwebcl.camst.it/XXXXXX/ITChefWebApp
+     * Puoi trovare questo url aprendo il portale Camst nel browser e copiando l'url fino a /ITChefWebApp
+     */
     camstBaseUrl: process.env.CAMST_BASEURL,
 };
